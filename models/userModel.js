d3fb638b242f86af4c6796d1f8125a19f96b7a6e
iamshushant <kumarshushant723@gmail.com>
2024-06-18 05:32:44 +0530

    Minor changes

--- models/userModel.js
+++ models/userModel.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     required: [true, "Role is required"],
-    enum: ["admin", "supervisor", "user"],
+    enum: ["admin", "supervisor"],
   },
 });
 
