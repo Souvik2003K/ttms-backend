a65af2d706e4eea2bae3a1e3a00b3a4e6913f4e3
iamshushant <kumarshushant723@gmail.com>
2025-05-14 13:05:47 +0530

    Added Allocated

--- models/Device.js
+++ models/Device.js
@@ -30,6 +30,11 @@ const deviceSchema = new mongoose.Schema({
     type: Number,
     default: null,
   },
+  allocated: {
+    type: Boolean,
+    default: false,
+  },
+
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
