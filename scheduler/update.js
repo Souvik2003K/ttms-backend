2861628fbaea52f491ef2b7a5734257c00089ca0
iamshushant <kumarshushant723@gmail.com>
2025-05-29 17:23:12 +0530

    Update Device cordinate spoof

--- scheduler/update.js
+++ scheduler/update.js
@@ -51,12 +51,42 @@ function generateRandomCoordinate() {
   };
 }
 
+function getRandomPointInTriangle(p1, p2, p3) {
+  let r1 = Math.random();
+  let r2 = Math.random();
+
+  if (r1 + r2 >= 1) {
+    r1 = 1 - r1;
+    r2 = 1 - r2;
+  }
+
+  const lat = p1.lat + r1 * (p2.lat - p1.lat) + r2 * (p3.lat - p1.lat);
+  const lng = p1.lng + r1 * (p2.lng - p1.lng) + r2 * (p3.lng - p1.lng);
+
+  return { latitude: lat, longitude: lng };
+}
+
+function generateRandomCoordinateInGeoRectangle() {
+  const A = { lat: 26.292257, lng: 78.233106 };
+  const B = { lat: 26.292382, lng: 78.232639 };
+  const C = { lat: 26.29293, lng: 78.232832 };
+  const D = { lat: 26.292844, lng: 78.233287 };
+
+  // Pick triangle ABC or ACD
+  if (Math.random() < 0.5) {
+    return getRandomPointInTriangle(A, B, C);
+  } else {
+    return getRandomPointInTriangle(A, C, D);
+  }
+}
+
 export const updateDeviceCoordinates = async () => {
   try {
     const devices = await Device.find();
 
     for (const device of devices) {
-      const newLocation = generateRandomCoordinate();
+      //const newLocation = generateRandomCoordinate();
+      const newLocation = generateRandomCoordinateInGeoRectangle();
       device.location = newLocation;
       device.updatedAt = new Date();
       await device.save(); // or Device.updateOne({ _id: device._id }, { location: newLocation });
